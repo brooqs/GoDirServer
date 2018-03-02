6f93e710c4881d67c3747c56ad0561f1c62e8087
brooqs <brooqs@gmail.com>
2018-03-02 17:06:51 +0300

    default port added
    default port added now can run without specify port number

--- godirserver.go
+++ godirserver.go
@@ -1,24 +1,28 @@
 package main
 
 import (
-  "log"
   "net/http"
   "os"
+  "fmt"
  )
 
 func main() {
-  preport := os.Args[1:]
+preport := os.Args[1:]
+fmt.Println("You can specify port as argument")
+  if len(preport) < 1 {
+    
+    port := ":8000"
+    fs := http.FileServer(http.Dir(""))
+    http.Handle("/", fs)
+    fmt.Println("There is no specific port using default. Serving port" + port + " Type your browser http://localhost" + port)
+    http.ListenAndServe(port, nil)
+    } else {
 
-  if len(preport) < 1{
-  	log.Println("No port is specified")
-  	log.Println("Usage: godirserv port e.g: godirserv 3000 Beware Use high port like 3000 10000 ")
-  } else{
-
-  port := ":" + preport[0]
-  fs := http.FileServer(http.Dir(""))
-  http.Handle("/", fs)
-  //fmt.Println(port[0])
-  log.Println("Serving port" + port + " Type your browser http://localhost" + port)
-  http.ListenAndServe(port, nil)
-}
+    port := ":" + preport[0]
+    fs := http.FileServer(http.Dir(""))
+    http.Handle("/", fs)
+    
+    fmt.Println("Port Specified Serving port" + port + " Type your browser http://localhost" + port)
+    http.ListenAndServe(port, nil)
+  }
 }
